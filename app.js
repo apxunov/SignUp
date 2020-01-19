17dd2d7af010e53fc491b377189f4c881f003f94
Mihail Arhipov <arhipov_ma@mail.ru>
2020-01-19 15:03:55 +0300

    added some features

--- app.js
+++ app.js
@@ -2,3 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.listen(3000, ()=> {
+    console.log("Server is running on port 3000")
+})
